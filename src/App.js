6f5b4dfcb850a54f66c6a06ada9ab3b3b59d3d5b
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2023-01-10 16:25:34 +0200

    u

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Welcome } from './pages/Welcome/Welcome';
+function App() {
+  return (
+    <div className="container">
+      <Routes>
+        <Route index path='miraplay' element={<Welcome/>} />
+        {/* <Route  path='/reviews' element={<Reviews/>}/> */}
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+import css from './Welcome.module.css'
+
+
+export const Welcome=()=>{
+
+
+    return(
+        <div className={css.container}>
+            <div className={css.imgWrap}></div>
+            WELCOMWE PAGE
+        </div>
+    )
+
+}
